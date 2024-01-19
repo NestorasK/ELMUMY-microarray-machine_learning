2c43ff54a26996dfd530087cc81914a79191d94d
NestorasK <nestor.karathanasis@gmail.com>
2024-01-19 14:19:51 +0200

    use norm expression from cel files, GPL96 platform

--- src/python/Script_collect_common_ids.py
+++ src/python/Script_collect_common_ids.py
@@ -1,25 +1,22 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from functools import reduce
 
 # I selected the microarray that are under the same 'GPL96' platform
 filenames = [
-    "data/raw/GSE2113/expression_data.csv",
-    "data/raw/GSE6477/expression_data.csv",
-    "data/raw/GSE13591/expression_data.csv",
-    "data/raw/GSE14230/expression_data.csv",
+    "data/raw/GSE2113/norm_expression_from_cel.csv",
+    "data/raw/GSE6477/norm_expression_from_cel.csv",
+    "data/raw/GSE13591/norm_expression_from_cel.csv",
+    "data/raw/GSE14230/norm_expression_from_cel.csv",
 ]
 
-path2savehists = "data/figures/"
 dfs = []
 for fi in filenames:
+    print(f"\nReading file: {fi}")
     dfi = pd.read_csv(filepath_or_buffer=fi)
-    print(dfi.shape)
+    print(dfi)
     dfs.append(dfi)
 
-
-common_rows = reduce(lambda left, right: pd.merge(left, right, on="ID"), dfs)
+common_rows = reduce(lambda left, right: pd.merge(left, right, on="rn"), dfs)
 print("Common Rows:")
 print(common_rows)
-print(common_rows.shape)
-common_rows.to_csv(path_or_buf="data/processed/gpl96_platform.csv")
+common_rows.to_csv(path_or_buf="data/processed/geoSup_gpl96_platform.csv")
