c5b67005ab2edb6e052d6e1a9be53f7b287801ef
NestorasK <nestor.karathanasis@gmail.com>
2024-08-12 17:30:19 +0300

    Compare models "GSE235356" and "allGPL96" training sets

---
+++ src/R/caret/progression/Interpretation/Script_enrichment_analysis_progression_GSE235356_vs_allGPL96.R
@@ -0,0 +1,299 @@
+rm(list = ls())
+library(data.table)
+library(ggplot2)
+library(clusterProfiler)
+library(ReactomePA)
+library(org.Hs.eg.db)
+library(annotate)
+library(hgu133plus2.db)
+library(httpgd)
+hgd()
+hgd_browse()
+
+path2save <- paste0(
+    "results/experiments_caret/multiple_myeloma_progression/",
+    "interpretation_optimizing_auc/enrichment_analysis/"
+)
+
+# Variable importance - ALL GPL96
+path2read <- paste0(
+    "results/experiments_caret/multiple_myeloma_progression/",
+    "interpretation_optimizing_auc/variable_importance/"
+)
+importance_all_gpl96 <- fread(
+    file = paste0(
+        path2read,
+        "variable_importance_caret.csv"
+    )
+)
+importance_all_gpl96$meta_train <- "allGLP96"
+importance_all_gpl96[
+    , myOverall := rowMeans(x = .SD, na.rm = TRUE),
+    .SDcols = c("MGUS", "MM", "Overall")
+]
+
+# Variable importance - onlyGSE235356
+path2read <- paste0(
+    "results/experiments_caret/multiple_myeloma_progression_onlyGSE235356/",
+    "interpretation_optimizing_auc/variable_importance/"
+)
+importance_onlyGSE235356 <- fread(
+    file = paste0(
+        path2read,
+        "variable_importance_caret.csv"
+    )
+)
+importance_onlyGSE235356$meta_train <- "GSE"
+importance_onlyGSE235356[
+    , myOverall := rowMeans(x = .SD, na.rm = TRUE),
+    .SDcols = c("MGUS", "progressing_MGUS", "Overall")
+]
+
+# Merge
+importance_all <- rbindlist(
+    l = list(
+        importance_all_gpl96[
+            , c("rn", "method", "transformation", "meta_train", "myOverall")
+        ],
+        importance_onlyGSE235356[
+            , c("rn", "method", "transformation", "meta_train", "myOverall")
+        ]
+    )
+)
+importance_all <- importance_all[order(-myOverall), ]
+importance_all <- importance_all[
+    transformation != "binary_0" & myOverall > 0 & method != "svmLinear2"
+]
+method_train <- unique(importance_all[, c("method", "meta_train")])
+
+probe_lists <- vector(
+    mode = "list",
+    length = nrow(method_train)
+)
+
+for (j in seq_len(nrow(method_train))) {
+    probe_lists[[j]] <- unique(
+        x = unlist(
+            strsplit(
+                x = importance_all[
+                    method == method_train[j, method] &
+                        meta_train == method_train[j, meta_train], rn
+                ],
+                split = "///", fixed = TRUE
+            )
+        )
+    )
+
+    # write file
+    dt <- data.table(
+        unique(
+            x = unlist(
+                strsplit(
+                    x = importance_all[
+                        method == method_train[j, method] &
+                            meta_train == method_train[j, meta_train], rn
+                    ],
+                    split = "///", fixed = TRUE
+                )
+            )
+        )
+    )
+    colnames(dt) <- paste0(
+        ">", method_train[j, method],
+        "_", method_train[j, meta_train]
+    )
+    fwrite(
+        x = dt,
+        file = paste0(
+            path2save,
+            method_train[j, method],
+            "_",
+            method_train[j, meta_train],
+            "_important_features.fasta"
+        ),
+        row.names = FALSE
+    )
+}
+names(probe_lists) <- paste0(
+    method_train[, method], "+", method_train[, meta_train]
+)
+
+# Convert probe IDs to gene symbols using the annotation package
+convert_probes_to_genes <- function(probes) {
+    # Get the gene symbols
+    gene_symbols <- unlist(mget(probes, hgu133plus2SYMBOL))
+    return(gene_symbols)
+}
+
+# Apply the conversion function to all lists
+gene_lists <- lapply(probe_lists, convert_probes_to_genes)
+
+# Convert gene symbols to Entrez IDs
+gene_lists_entrez <- lapply(gene_lists, function(genes) {
+    bitr(genes,
+        fromType = "SYMBOL",
+        toType = "ENTREZID", OrgDb = org.Hs.eg.db
+    )$ENTREZID
+})
+
+# Perform enrichment analysis
+go_results <- compareCluster(
+    geneCluster = gene_lists_entrez,
+    fun = "enrichGO",
+    OrgDb = org.Hs.eg.db,
+    ont = "BP", # Biological Process
+    pAdjustMethod = "BH",
+    pvalueCutoff = 0.05,
+    qvalueCutoff = 0.2
+)
+fwrite(
+    x = as.data.table(go_results),
+    file = paste0(path2save, "enrichGO_results - GSE235356 vs allGLP96.csv")
+)
+
+# enrich pathways
+pathway_results <- compareCluster(
+    geneCluster = gene_lists_entrez,
+    fun = ReactomePA::enrichPathway,
+    organism = "human", # Homo sapiens
+    pAdjustMethod = "BH",
+    pvalueCutoff = 0.05,
+    qvalueCutoff = 0.2,
+    readable = TRUE
+)
+fwrite(
+    x = as.data.table(pathway_results),
+    file = paste0(
+        path2save, "enrichPathway_results - GSE235356 vs allGLP96.csv"
+    )
+)
+
+# KEGG pathway
+kegg_results <- compareCluster(
+    geneCluster = gene_lists_entrez,
+    fun = "enrichKEGG",
+    organism = "hsa", # Homo sapiens
+    pAdjustMethod = "BH",
+    pvalueCutoff = 0.05,
+    qvalueCutoff = 0.2
+)
+fwrite(
+    x = as.data.table(kegg_results),
+    file = paste0(path2save, "enrichKEGG_results - GSE235356 vs allGLP96.csv")
+)
+
+# Disease Ontology Enrichment Analysis
+do_results <- compareCluster(
+    geneCluster = gene_lists_entrez,
+    fun = "enrichDO",
+    ont = "DO",
+    pAdjustMethod = "BH",
+    pvalueCutoff = 0.05,
+    qvalueCutoff = 0.2
+)
+fwrite(
+    x = as.data.table(do_results),
+    file = paste0(path2save, "enrichDO_results - GSE235356 vs allGLP96.csv")
+)
+
+# View results
+terms_to_check <- paste0(
+    "MAPK|RAS|RAF|MEK|ERK|PI3K|AKT|NF-KB|",
+    "STAT|Wnt|Hedgehog|TNFa|mTOR",
+    "myeloid|leukemia|myeloma|cancer"
+)
+
+# Visualize GO Enrichment Analysis
+dotplot(go_results) + ggtitle("GO Enrichment Analysis")
+# Specific GO terms
+go_results_df <- as.data.frame(go_results)
+specific_go_results_df <- go_results_df[
+    grepl(
+        pattern = terms_to_check, x = go_results_df$Description,
+        ignore.case = TRUE
+    ),
+]
+specific_go_results <- new(
+    "compareClusterResult",
+    compareClusterResult = specific_go_results_df
+)
+go_plot <- dotplot(specific_go_results) +
+    ggtitle("Selected GO Enrichment Analysis")
+ggsave(
+    filename = paste0(
+        path2save,
+        "Selected GO Enrichment Analysis - GSE235356 vs allGLP96.pdf"
+    ),
+    plot = go_plot, width = 15, height = 13
+)
+
+# Visualize Pathway Enrichment Analysis Results
+pathway_results_df <- as.data.frame(pathway_results)
+selected_pathway_results <- new(
+    "compareClusterResult",
+    compareClusterResult = pathway_results_df[
+        grepl(
+            pattern = terms_to_check,
+            x = pathway_results@compareClusterResult$Description,
+            ignore.case = TRUE
+        ),
+    ]
+)
+pathway_plot <- dotplot(selected_pathway_results) +
+    ggtitle("Selected Reactome Pathways")
+ggsave(
+    filename = paste0(
+        path2save,
+        "Selected Pathway Enrichment Analysis - GSE235356 vs allGLP96.pdf"
+    ),
+    plot = pathway_plot, width = 14, height = 6
+)
+
+# Visualize KEGG Pathway Enrichment Results
+kegg_df <- as.data.frame(kegg_results)
+dotplot(kegg_results) + ggtitle("KEGG Pathway Enrichment Analysis")
+
+specific_kegg_df <- kegg_df[
+    grepl(
+        pattern = terms_to_check,
+        x = kegg_df$Description
+    ),
+]
+specific_kegg_results <- new(
+    "compareClusterResult",
+    compareClusterResult = specific_kegg_df
+)
+kegg_plot <- dotplot(specific_kegg_results) +
+    ggtitle("Selected KEGG Pathway Enrichment Analysis")
+ggsave(
+    filename = paste0(
+        path2save,
+        "Selected KEGG Pathway Enrichment Analysis - GSE235356 vs allGLP96.pdf"
+    ),
+    plot = kegg_plot, width = 15, height = 5
+)
+
+# Visualize Disease Ontology Enrichment Results
+dotplot(do_results) + ggtitle("Disease Ontology Enrichment Analysis")
+
+# Focus on specific diseases
+do_results_df <- as.data.frame(do_results)
+inds <- grepl(
+    pattern = terms_to_check,
+    x = do_results_df$Description
+)
+specific_do_results_df <- do_results_df[inds, ]
+specific_do_results <- new(
+    "compareClusterResult",
+    compareClusterResult = specific_do_results_df
+)
+do_plot <- dotplot(specific_do_results) + ggtitle(
+    "Selected Disease Ontology Enrichment Analysis"
+)
+ggsave(
+    filename = paste0(
+        path2save,
+        "Selected Disease Ontology Enrichment Analysis - GSE235356 vs allGLP96.pdf"
+    ),
+    plot = do_plot, width = 15, height = 6
+)
---
+++ src/R/caret/progression/Interpretation/Script_number_of_features_GSE235356_vs_allGPL96.R
@@ -0,0 +1,184 @@
+rm(list = ls())
+library(data.table)
+library(ggplot2)
+library(pheatmap)
+library(VennDiagram)
+library(httpgd)
+hgd()
+hgd_browse()
+
+
+path2save <- paste0(
+    "results/experiments_caret/multiple_myeloma_progression/",
+    "interpretation_optimizing_auc/variable_importance/"
+)
+
+# Variable importance - ALL GPL96
+path2read <- paste0(
+    "results/experiments_caret/multiple_myeloma_progression/",
+    "interpretation_optimizing_auc/variable_importance/"
+)
+importance_all_gpl96 <- fread(
+    file = paste0(
+        path2read,
+        "variable_importance_caret.csv"
+    )
+)
+importance_all_gpl96$meta_train <- "all_GLP96"
+importance_all_gpl96[
+    , myOverall := rowMeans(x = .SD, na.rm = TRUE),
+    .SDcols = c("MGUS", "MM", "Overall")
+]
+
+
+# Variable importance - onlyGSE235356
+path2read <- paste0(
+    "results/experiments_caret/multiple_myeloma_progression_onlyGSE235356/",
+    "interpretation_optimizing_auc/variable_importance/"
+)
+importance_onlyGSE235356 <- fread(
+    file = paste0(
+        path2read,
+        "variable_importance_caret.csv"
+    )
+)
+colnames(importance_onlyGSE235356)
+importance_onlyGSE235356$meta_train <- "GSE235356"
+importance_onlyGSE235356[
+    , myOverall := rowMeans(x = .SD, na.rm = TRUE),
+    .SDcols = c("MGUS", "progressing_MGUS", "Overall")
+]
+
+# Merge
+importance_all <- rbindlist(
+    l = list(
+        importance_all_gpl96[
+            , c("rn", "method", "transformation", "meta_train", "myOverall")
+        ],
+        importance_onlyGSE235356[
+            , c("rn", "method", "transformation", "meta_train", "myOverall")
+        ]
+    )
+)
+numfeatures <- importance_all[
+    transformation != "binary_0",
+    .(nfeatures = sum(myOverall > 0)),
+    by = c("method", "transformation", "meta_train")
+]
+
+set.seed(123)
+dot_plot_nimpfeat <- ggplot(
+    data = numfeatures,
+    mapping = aes(x = method, y = nfeatures, colour = transformation)
+) +
+    geom_jitter(width = 0.1, height = 0, shape = 1) +
+    xlab("") +
+    ylab("Number of features") +
+    ggtitle(
+        label = "Number of features in the models"
+    ) +
+    # coord_flip() +
+    theme(
+        axis.text.x = element_text(angle = 60, hjust = 1),
+        legend.position = "bottom"
+    ) +
+    facet_wrap(facets = vars(meta_train))
+ggsave(
+    filename = paste0(
+        path2save,
+        "dotplot_num_imp_features_myOverall_onlyGSE_vs_allGPL96.pdf"
+    ),
+    plot = dot_plot_nimpfeat, width = 6, height = 4
+)
+
+# Number of common features
+number_of_common_probes <- unique(
+    importance_all[
+        (transformation != "binary_0") &
+            (method %in% c("glmnet", "rf", "gbm")) &
+            (myOverall > 0), .(number_of_methods = length(unique(meta_train))),
+        by = c("transformation", "rn", "method")
+    ]
+)
+number_of_common_probes[, method_transf := paste0(method, "+", transformation)]
+
+number_of_methods <- rbindlist(
+    l = lapply(
+        X = unique(number_of_common_probes$method_transf),
+        FUN = function(trj, mati = number_of_common_probes) {
+            dt <- data.table(
+                table(mati[method_transf == trj, number_of_methods])
+            )
+            dt$method_transf <- trj
+            return(dt)
+        }
+    )
+)
+colnames(number_of_methods)[c(1, 2)] <- c(
+    "number_of_trainingsets", "number_of_probes"
+)
+number_of_methods[
+    , c("method", "transformation") := tstrsplit(
+        x = method_transf,
+        split = "+", keep = c(1, 2), fixed = TRUE
+    )
+]
+set.seed(42)
+dotplot_commonprobes_methods <- ggplot(
+    data = number_of_methods[number_of_trainingsets == 2, ],
+    mapping = aes(
+        x = method,
+        y = number_of_probes, colour = transformation
+    )
+) +
+    geom_jitter(width = 0.1, height = 0, shape = 1) +
+    ylab("Number of common probes") +
+    xlab("") +
+    ggtitle(
+        label = "Number of common probes across training sets",
+        subtitle = "All GPL96 | GSE235356"
+    )
+ggsave(
+    filename = paste0(
+        path2save,
+        "Number of common probes across onlyGSE_vs_allGPL96.pdf"
+    ),
+    plot = dotplot_commonprobes_methods,
+    width = 6, height = 3.5
+)
+
+# Venn diagrams ####
+for (trj in unique(importance_all[, transformation])) {
+    dtj <- dcast.data.table(
+        data = importance_all[(transformation == trj) & (myOverall != 0)],
+        formula = rn + method ~ meta_train,
+        value.var = "myOverall",
+        fun.aggregate = length
+    )
+    for (mi in unique(dtj[, method])) {
+        dtj_mi <- dtj[method == mi, ]
+        vennplot <- venn.diagram(
+            x = list(
+                GSE235356 = which(dtj_mi$GSE235356 > 0),
+                GLP96 = which(dtj_mi$all_GLP96 > 0)
+            ),
+            filename = NULL,
+            main = paste0(
+                "Number of common probes across training sets\n",
+                "Method:", mi, " | Transformations:", trj
+            )
+        )
+        pdf(
+            file = paste0(
+                path2save,
+                "venn_diagram_common_features_across_trainingsets_",
+                trj, "_transformation_",
+                mi, "_method",
+                ".pdf"
+            ),
+            width = 4.5, height = 4
+        )
+        grid.draw(vennplot)
+        dev.off()
+    }
+}
